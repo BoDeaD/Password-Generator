6bedc7e6ab54ff06709af58810e976c8d2195ab4
BoDeaD <lageroped7@gmail.com>
2023-02-24 18:49:08 +0200

    change logotype photo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class App(CTk.CTk):
         self.title("Password Generator")
         self.resizable(False,False)
 
-        self.logo = CTk.CTkImage(dark_image = Image.open("LogoPass_G.png"), size= (460,150))
+        self.logo = CTk.CTkImage(dark_image = Image.open("logo.png"), size= (460,150))
         self.logo_label = CTk.CTkLabel(master = self, text = "", image = self.logo)
         self.logo_label.grid(row = 0, column = 0)
 
@@ -24,7 +24,7 @@ class App(CTk.CTk):
 
         self.btn_generate = CTk.CTkButton(master=self.password_frame,text = "Generate", width=100,
                                           command=self.set_password)
-        self.btn_generate.grid(row = 0,column = 1)
+        self.btn_generate.grid(row = 0,column = 1, pady = (10,10))
 
         self.settings_frame = CTk.CTkFrame(master=self)
         self.settings_frame.grid(row = 2, column = 0, padx =(20,20),pady = (20,0),sticky = "nsew")
