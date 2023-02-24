d011218368b194ca2639edc0d283f9b73beef869
BoDeaD <lageroped7@gmail.com>
2023-02-24 12:59:40 +0200

    add slider and other tips

--- main.py
+++ main.py
@@ -23,6 +23,21 @@ class App(CTk.CTk):
                                           command=self.set_password)
         self.btn_generate.grid(row = 0,column = 1)
 
+        self.settings_frame = CTk.CTkFrame(master=self)
+        self.settings_frame.grid(row = 2, column = 0, padx =(20,20),pady = (20,0),sticky = "nsew")
+
+        self.password_lengh_slider = CTk.CTkSlider(master=self.settings_frame, from_=1, to=50, number_of_steps=50,
+                                                   command=self.slider_event)
+        self.password_lengh_slider.grid(row=1, column=0,columnspan= 3,pady = (20,20), sticky="ew")
+
+        self.password_lengh_entry = CTk.CTkEntry(master=self.settings_frame,width=50)
+        self.password_lengh_slider.grid(row=1, column= 3,padx= (20,10),sticky="we")
+
+        
+
+    def slider_event(self):
+        pass
+
     def set_password(self):
         pass
 
